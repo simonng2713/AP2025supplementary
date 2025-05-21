4b5fead7816df6e935f0798462e1e74a108e1832
Simon Ng <simon.kah.seng@misc.mech.eng.osaka-u.ac.jp>
2025-05-21 12:16:22 +0900

    20250521

---
+++ code/lesson05/classtest_div/CMakeFiles/classtest_div.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Allin/Desktop/AP_lecture/AP Supplementary/AP2025supplementary/code/lesson05/classtest_div/Rectangle.cpp" "CMakeFiles/classtest_div.dir/Rectangle.cpp.o" "gcc" "CMakeFiles/classtest_div.dir/Rectangle.cpp.o.d"
+  "/mnt/c/Users/Allin/Desktop/AP_lecture/AP Supplementary/AP2025supplementary/code/lesson05/classtest_div/classtest_div.cpp" "CMakeFiles/classtest_div.dir/classtest_div.cpp.o" "gcc" "CMakeFiles/classtest_div.dir/classtest_div.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/lesson05/classtest_div/CMakeFiles/classtest_div.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/classtest_div.dir/Rectangle.cpp.o"
+  "CMakeFiles/classtest_div.dir/Rectangle.cpp.o.d"
+  "CMakeFiles/classtest_div.dir/classtest_div.cpp.o"
+  "CMakeFiles/classtest_div.dir/classtest_div.cpp.o.d"
+  "classtest_div"
+  "classtest_div.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/classtest_div.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
